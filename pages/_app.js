b0a5a2bf5648fe00711f48ee51b0358ba8161006
Avadhut <avdhutshinde22@gmail.com>
2022-12-23 12:47:54 +0530

    adding student page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, ChakraProvider, Image } from '@chakra-ui/react'
 import HambergerNavbar from '../Components/Navbar'
 import '../styles/globals.css'
+import io from 'socket.io-client';
+export const socket = io.connect('http://localhost:8080');
 
 export default function App({ Component, pageProps }) {
+
   return (
     <ChakraProvider>
-      <HambergerNavbar />
+      {/* <HambergerNavbar /> */}
+      {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.5.4/socket.io.min.js"></script> */}
+  
       <Component {...pageProps} />
     </ChakraProvider>
   )
