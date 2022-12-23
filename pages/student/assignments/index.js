6e5075a813dd4555f9cfd23ef95aa37996bea89a
Avadhut <avdhutshinde22@gmail.com>
2022-12-23 15:16:20 +0530

    student page

--- pages/student/assignments/index.js
+++ pages/student/assignments/index.js
@@ -2,7 +2,7 @@ import { Box, Button, Heading, Image } from "@chakra-ui/react";
 import { useState } from "react";
 import Navbarstu from "../../../Components/college/Navbarstu";
 import {lec} from "../../../db.json"
-export default function Assi(){
+export default function Assi({data}){
     const[state,setstate]=useState(false)
     function toggle(){
 
@@ -11,9 +11,9 @@ export default function Assi(){
         <Box >
         <Navbarstu/>
 <Box w="80%" m="auto" pt="100px">
-{lec.map((ele)=>
+{data?.map((ele)=>
 <Box pb="20px" pt="20px" pl="20px" pr="20px" mb="30px" boxShadow='md' mt="30px" bgColor="white" _hover={{cursor:"pointer"}} display="flex" justifyContent="space-between">
-<Heading fontSize="20px" color="blue.400">{ele.lec}</Heading>
+<Heading fontSize="20px" color="blue.400">{ele.title}</Heading>
 
 <Button onClick={toggle}>pending</Button>
 </Box>
@@ -22,3 +22,16 @@ export default function Assi(){
     </Box>
     )
 }
+export async function getServerSideProps(context) {
+    const res=await fetch("https://nervous-baseball-cap-tuna.cyclic.app/admin",{
+        method: 'POST',
+        body: JSON.stringify({type:"assignment"}),
+        headers: { 'Content-Type': 'application/json' }
+    
+        });
+        const data=await res.json();
+        console.log(data)
+ return {
+      props: {data}, // will be passed to the page component as props
+    }
+  }
